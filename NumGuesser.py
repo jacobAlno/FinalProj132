e9e39b12047679dd24dad615a8a9e970dbb677ab
Jacob Alnoor <jacoba267@icloud.com>
2026-04-28 17:57:47 -0400

    get proper guess

--- NumGuesser.py
+++ NumGuesser.py
@@ -6,4 +6,19 @@ def game_loop():
 
     print("Guess whole number from 1 to 100")
 
-    #while True:
+    while True:
+        rawGuess = input("Enter Guess: ").strip()
+
+        if not rawGuess.isdigit():
+            print("Enter a valid nonzero positive int")
+            continue
+
+        guess = int(rawGuess)
+        attempts += 1
+        
+        if guess < 1 or guess > 100:
+            print("Your guess must be between 1 and 100.\n")
+            attempts -= 1
+            continue
+
+        
